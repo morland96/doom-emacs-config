de00c14b4d651242c845625e88d5750c7f2e0a54
Morland Meng <mmeng@atlassian.com>
2023-04-21 16:33:31 +1000

    init

---
+++ config.el
@@ -0,0 +1,177 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "Morland Meng"
+      user-mail-address "mmeng@atlassian.com"
+      which-key-idle-delay 0.5
+      which-key-idle-secondary-delay 0)
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+(setq doom-font (font-spec :family "Menlo" :size 14)
+      doom-variable-pitch-font (font-spec :family "MesloLGS NF" :size 13)
+      doom-unicode-font (font-spec :family "MesloLGS NF" :size 13))
+(setq default-text-properties '(line-height 1.4))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+;; Set initial frame size and position
+
+;; -- UI --
+(setq default-frame-alist '((fullscreen . maximized)))
+(vertico-posframe-mode 1)
+;;(helm-posframe-enable)
+
+(with-eval-after-load 'doom-themes
+  (doom-themes-treemacs-config))
+(setq doom-themes-treemacs-theme "doom-colors")
+
+;; -- TABS --
+(after! centaur-tabs
+  (setq centaur-tabs-style "wave")
+  (setq centaur-tabs-set-bar 'under)
+  (+popup-window-p)
+  (centaur-tabs-group-by-projectile-project))
+(defun centaur-tabs-hide-tab (x)
+  "Do no to show buffer X in tabs."
+  (let ((name (format "%s" x)))
+    (or
+     ;; Current window is not dedicated window.
+     (window-dedicated-p (selected-window))
+
+     ;; Buffer name not match below blacklist.
+     (string-prefix-p "*epc" name)
+     (string-prefix-p "*helm" name)
+     (string-prefix-p "*Helm" name)
+     (string-prefix-p "*Compile-Log*" name)
+     (string-prefix-p "*lsp" name)
+     (string-prefix-p "*company" name)
+     (string-prefix-p "*Flycheck" name)
+     (string-prefix-p "*tramp" name)
+     (string-prefix-p " *Mini" name)
+     (string-prefix-p "*help" name)
+     (string-prefix-p "*straight" name)
+     (string-prefix-p " *temp" name)
+     (string-prefix-p "*vterm" name)
+     (string-prefix-p "*Help" name)
+     (string-prefix-p "*mybuf" name)
+
+     ;; Is not magit buffer.
+     (and (string-prefix-p "magit" name)
+          (not (file-name-extension name)))
+     )))
+
+;; -- Projects --
+(after! projectile
+  (setq projectile-project-root-files-bottom-up '("package.json" ".projectile" ".project" ".git")
+        projectile-ignored-projects '("~/.emacs.d/")
+        projectile-project-search-path '("~/projects" "~/playground")))
+
+;; -- FRINGE --
+;; (add-hook! 'doom-init-ui-hook (fringe-mode nil))
+;; ;;(setq left-fringe-width 32)
+;; (add-hook! +dap-running-session-mode
+  ;; (set-window-buffer nil (current-buffer)))
+;; (set-fringe-style (quote (20 . 10)))
+(after! doom-themes-ext-treemacs
+  (with-eval-after-load 'treemacs
+    (remove-hook 'treemacs-mode-hook #'doom-themes-hide-fringes-maybe)
+    (advice-remove #'treemacs-select-window #'doom-themes-hide-fringes-maybe)))
+;;(set-fringe-style (quote (12 . 8)))
+
+;; -- KEYS --
+(map! :leader "!" #'vterm)
+(map! :leader "jt" #'centaur-tabs-ace-jump)
+
+;; -- RUST --
+(require 'dap-lldb)
+(require 'dap-codelldb)
+(require 'dap-cpptools)
+;;(setq dap-lldb-debug-program `("/Users/mmeng/.vscode/extensions/vadimcn.vscode-lldb-1.9.0/lldb/bin/lldb"))
+
+(setq lsp-rust-analyzer-debug-lens-extra-dap-args
+        `(:MIMode "lldb"
+                :miDebuggerPath "lldb-mi"
+                :stopAtEntry t
+                :externalConsole
+                :json-false))
+
+(dap-register-debug-template
+        "Rust::LLDB Run Configuration"
+        (list :type "lldb"
+                :request "launch"
+                :name "LLDB::Run"
+          :gdbpath "rust-lldb"
+          :cwd (lsp-workspace-root)
+        ))
+
+;; -- JAVA --
+(setq lsp-java-vmargs
+      '("-XX:+UseParallelGC" "-XX:GCTimeRatio=4" "-XX:AdaptiveSizePolicyWeight=90" "-Dsun.zip.disableMemoryMapping=true" "-Xmx4G" "-Xms100m"))
